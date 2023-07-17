210d884cc29193223642e5ace6441e6615d7f4c3
kushagra208 <kushagrag2082002@gmail.com>
2023-07-17 09:37:19 +0530

    remove waste code

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
 export const app = express();
 
 app.use(express.json({ limit: "50mb" }));
@@ -9,9 +8,3 @@ app.use(cookieParser());
 
 import { userRouter } from "./routes/User.js";
 app.use("/api/v1", userRouter);
-
-app.use(express.static(path.resolve("./frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("./frontend/build/index.html"));
-});
