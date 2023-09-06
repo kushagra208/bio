629bf1924cd655e6a00db0595ef5ac4c44086576
kushagra208 <kushagrag2082002@gmail.com>
2023-09-06 19:51:26 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 export const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-
+app.use(cors());
 import { userRouter } from "./routes/User.js";
 app.use("/api/v1", userRouter);
